5fc66ffc87ebaeddd7320f8fa174dd5c2aab33fa
SimoneAcconcia <113896848+SimoneAcconcia@users.noreply.github.com>
2022-09-22 10:00:57 +0200

    Update esercitazione19.c

--- 001-matrice/esercitazione19.c
+++ 001-matrice/esercitazione19.c
@@ -3,93 +3,119 @@ Acconcia Simone        *
 4H                     *
 15/09/2022             *
 **********************/
+//dichiarazione librerie e costanti
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <limits.h>
-#define R 3
-#define C 3
-#define max 100
-#define min 0
+#include <stdio.h> 		       // libreria input/output
+#include <stdlib.h>           //
+#include <time.h>  		     // libreria per generare numeri casuali
+#include <limits.h> 	    // contiene  due costanti INT_MAX, INT_MIN
+#define R 3 			   // costante per le righe della matrice
+#define C 3 	 	  //costante per le colonne della matrice
+#define max 100  	  	 // costante per generare il massimo numero casuale 
+#define min 0			// costante per generare il minimo numero casuale 
 
 
 //funzione che carica il vettore con numeri casuali
-void carica_randomvettore(int vet[],int dim);
+void carica_randomvettore(int vet[],int dim); //parametri: vettore e dimensione vettore
 
 //richiama la funzione carica vettore per caricare la matrice											
-void carica_randommat(int mat[][C], int righe,int colonne);
+void carica_randommat(int mat[][C], int colonne);//parametri: matrice e dimensione colonne
 
 //funzione stampa
-void stampa(int mat[][C],int r,int c);
+void stampa(int mat[][C],int r,int c); //parametri: matrice e dimensione righe e colonne
 
 //funzione che carica il vettore da tastiera
-void carica_manualevet(int vet[],int dim);
+void carica_manualevet(int vet[],int dim); //parametri: vettore e dimensione vettore
 
 //funzione che richiama la carica da tastiera del vettore
-void carica_manualemat(int mat[][C] ,int colonne);
+void carica_manualemat(int mat[][C] ,int colonne);  //parametri: matrice e dimensione colonne
 
 //ordina in ordine crescente il vettore
-void ordinamento_vettore(int vet[],int dim);
+void ordinamento_vettore(int vet[],int dim);//parametri: vettore e dimensione vettore
 
 //ordina le righe della riga chiesta dall'utente della matrice
-void ordinamento_conriga(int mat[][C] ,int r,int c);
+void ordinamento_conriga(int mat[][C] ,int r,int c); //parametri: matrice e dimensione righe e colonne
 
 //ricerca maggiore
-int maggiore(int mat[][C],int riga,int colonne);
+int maggiore(int mat[][C],int riga,int colonne); //parametri: matrice e dimensione righe e colonne
 
 //ricerca minore
-int minore(int mat[][C],int riga,int colonne);
+int minore(int mat[][C],int riga,int colonne); //parametri: matrice e dimensione righe e colonne
 
 //inserimento ordinato ogni volta che si inserisce un numero il vettore è ordinato
-void inserimento_ordinato(int vet[],int r);
+void inserimento_ordinato(int vet[],int r); //parametri: vettore e dimensione vettore
 
 //ricerca un numero all'interno del vettore la riga è scelta dall'utente
-int ricercaingenua(int vet[],int dim,int x);
+int ricercaingenua(int vet[],int dim,int x); //parametri: vettore ,dimensione vettore ,numero da ricercare all'interno del vettore
+
 //funzioni colonne
-void caricaColrandom(int mat[][C],int r,int x);
-void caricaColtastiera(int mat[][C],int r,int x);
-void stampaCol(int mat[][C],int r,int x);
+
+void caricaColrandom(int mat[][C],int r,int x); //carica colonna
+
+void caricaColtastiera(int mat[][C],int r,int x); // carica tastiera
+
+void stampaCol(int mat[][C],int r,int x); // stampa colonna
+
 int ricercaCol(int mat[][C],int r,int x,int y); // funzione ricerca su una colonna indicata 
+
 int main()
 {
 
 	int matrice[R][C],a,b,r;
+	
+    // carica randon   
 	srand(time(NULL));
 	printf("\n\n");
     printf("generazione numeri casuali matrice\n\n");
-    carica_randommat(matrice,R,C);
+    carica_randommat(matrice,C);
     stampa(matrice,R,C);
-    //carica_manualemat(matrice,C);
-    //stampa(matrice);
-    printf("inserisci la riga su cui vuoi ricercare l'elemento\n'");
-    scanf("%d",&a);
+    
+    // carica manuale   
+    printf("inserisci dei numeri\n\n");
+    carica_manualemat(matrice,C);
+    stampa(matrice,R,C);
+    
+    // ricerca ingenua su riga
+    printf("inserisci la riga su cui vuoi ricercare l'elemento\n");
+    scanf("%d\n",&a);
     printf("inserisci il numero da cercare\n");
-    scanf("%d",&b);
+    scanf("%d\n",&b);
     r=ricercaingenua(matrice[a],R,b);
-    printf("%d",r);
+    printf("%d\n",r);
+    
+    
+    //ordinamento per riga
     printf("inserisci la riga su cui vuoi riordinare\n'");
-    scanf("%d",&a);
+    scanf("%d\n",&a);
     ordinamento_conriga(matrice,a,C); 
     stampa(matrice,R,C);
-  
-  	r=maggiore(matrice,R,C);
+    
+    //ricerca maggiore e minore nella matrice
+    r=maggiore(matrice,R,C);
   	printf(" il maggiore:%d\n",r);
   	r=minore(matrice,R,C),
     printf(" il minore:%d\n",r);
+    
+    // caricamento su colonna
     printf("inserisci la colonna su cui vuoi caricare la matrice\n");
-    scanf("%d",&a);
+    scanf("%d\n",&a);
     caricaColrandom( matrice,R,a);
     stampa(matrice,R,C);
+    
+    //carica colonna da tastiera
     printf("inserisci la colonna su cui vuoi caricare la matrice\n");
-    scanf("%d",&a);
+    scanf("%d\n",&a);
     caricaColtastiera(matrice,R,a);
     stampa(matrice,R,C);
+    
+    //stampa colonna 
 	printf("inserisci la colonna che vuoi stampare\n");
 	scanf("%d",&a);
 	stampaCol(matrice,R,a);
+	
+	// ricerca ingenua su colonna
 	printf("inserisci la colonna dove effettuare la ricerca\n e il numero da cercare\n");
-	scanf("%d%d",&a,&b);
+	scanf("%d\n%d\n",&a,&b);
 	r=ricercaCol(matrice,R,a,b);
 	printf("iil numero e' in posizione :'%d",r);
 	//inserimento_ordinato(matrice,R);
@@ -111,13 +137,13 @@ void stampa(int mat[][C],int r,int c)
 	{
 	 for(j=0;j<c;j++)
 	 {
-	 	printf("%d\n",mat[i][j]);
-	 }
+	 	printf("%d ",mat[i][j]);
+	 } printf("\n");
 		
 	}
 	
 }
-void carica_randommat(int mat[][C], int righe,int colonne)
+void carica_randommat(int mat[][C], int colonne)
 {
 	int i;
 	for(i=0;i<colonne;i++)
@@ -131,11 +157,11 @@ void carica_randommat(int mat[][C], int righe,int colonne)
 void carica_manualevet(int vet[],int dim)
 {
 	int  i;
-	printf("inserisci un numero");
+	
 	for(i=0;i<dim;i++)
 	{
 		
-		scanf("%d",&vet[i]);
+		scanf("%d\n",&vet[i]);
 		
 	}
 }
@@ -278,24 +304,3 @@ int ricercaCol(int mat[][C],int r,int x,int y)
 	
 }
 
-//void inserimento_ordinato(int vet[],int r)
-{
-		
-	int i,temp=0,j=0;
-
-	for(i=1;i<10;i++)
-	{ 
-            temp=vet[i]; 
-            j=i-1; 
-            while(j>=0 && vet[j]>temp){
-		vet[j+1]=vet[j];		
-		j--;		
-	}
-	vet[j+1]=temp;
-	}
-
-	for(i=0;i<10;i++){
-		printf("%d \t", vet[i]);
-	}
-		
-}
