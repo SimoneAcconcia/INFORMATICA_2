8fe75728cad6be03fc1ee54ee9ed1e9da34e95d1
SimoneAcconcia <simoneacconcia165@gmail.com>
2022-12-15 08:31:17 +0000

    update

---
+++ file a.c
@@ -0,0 +1,17 @@
+#include <stdio.h>                  
+#include <stdlib.h>
+#include <string.h>
+void inserisciRecord(char fileName[], int numRecord); /*inserisce in coda n record con i dati richiesti all’utente. Se il file non esiste va creato 
+                                                        e quindi effettuare l’inserimento. 
+                                                    */ 
+
+
+int main()
+{
+    FILE *f1;
+    ch
+    f1=fopen()
+
+
+
+}
