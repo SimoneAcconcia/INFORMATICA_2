e32531f52b8762b912497d78cf2a8a4f32e429d3
SimoneAcconcia <simoneacconcia165@gmail.com>
2022-12-18 17:08:10 +0000

    upgrade

--- 03-file bina/file a.c
+++ 03-file bina/file a.c
@@ -1,11 +1,19 @@
-#include <stdio.h>                  
-#include <stdlib.h>
-#include <string.h>
-#define S 50
-#define V 3
-#define MAX 50
+#include <stdio.h>       //inclusione libreria per input output           
+#include <stdlib.h>	    /*questa libreria permette l'uso delle funzioni di altre librerie
+						com time.h,limits.h
+                        */
+#include <string.h>     //inclusione libreria per le stringhe
 
-struct data
+//define
+#define S 50            //costante con 50 utilizzata per definire spazio delle stringhe
+
+#define V 3             //costante usata per l'array voti contenuto in struct studente
+
+#define MAX 50          //costante con 50 utilizzata per definire spazio delle stringhe
+
+//struct
+
+struct data              //struct data contiene informazioni sulla data di nascita
 {
     int giorno;
     char mese[S];
@@ -16,7 +24,7 @@ struct data
 
 
 
-struct studente
+struct studente         //contiene informazioni riguardanti lo studente    
 {
     int matricola;
     char cognome[S];
@@ -34,9 +42,11 @@ void inserisciRecord(char fileName[], int numRecord); /*inserisce in coda n reco
 
 int main()
 {
-    FILE *f1;
-    char NomeFile[MAX] ={"file.txt"};
-    inserisciRecord(NomeFile,1);
+    FILE *f1;                                   //puntatore al file
+
+    char NomeFile[MAX] ={"file.txt"};           //stringa usata per contenere il nome del file 
+
+    inserisciRecord(NomeFile,1);                //richiamo della funzione inserisciRecord
     
 
 
@@ -46,29 +56,48 @@ int main()
 void inserisciRecord(char fileName[], int numRecord) // fwrite
 {
     FILE *f1;
-    struct studente buffer;
-    f1=fopen(fileName,"wb");
+
+    struct studente buffer;             //dichiarazione della variabile buffer di tipo struct studente
+
+    f1=fopen(fileName,"wb");            //apertura del file binario in scrittura
+
     int j=0;
-    if(f1!=0)
+
+    if(f1!=0)                           //controllo degli errori sull apertura file
     {
+
+        //inserimento di tutti i campi di struct data e studente
         printf("inserisci matricola\n");
+
         scanf("%d",&buffer.matricola);
+
         printf("\n");
+
         printf("inserisci cognome\n");
+
         scanf("%s",buffer.cognome);
+
         for(j=0;j<V;j++)
         {
             printf("inserisci voti\n");
             scanf("%d",&buffer.voti[j]);
 
         }
-         printf("inserisci giorno\n");
+
+        printf("inserisci giorno\n");
+
         scanf("%d",&buffer.nascita.giorno);
+
         printf("inserisci mese\n");
+
         scanf("%s",&buffer.nascita.mese);
+
         printf("inserisci anno\n");
+
         scanf("%d",&buffer.nascita.anno);
+
         fwrite(&buffer,sizeof(struct studente),1,f1);
+        
 		fclose(f1);
     }
 
